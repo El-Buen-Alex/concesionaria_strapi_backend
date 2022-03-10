f39f454d8a40f8e0758188e992900c81402195d8
Daniel Pincay <alexispincay005@gmail.com>
2022-03-09 23:57:28 -0500

    integrando stripe

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,8 +1,36 @@
 'use strict';
+    
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+const stripe = require('stripe')(process.env.STRIPE_KEY)
+const MY_DOMAIN = 'http://localhost:3000/cart';
+module.exports={
+    async create(ctx){
+        const { cartDetail, cartTotal } = ctx.request.body
+        const line_items = cartDetail.map((cartItem) => {
+            const item = {}
+            item.price_data = {
+                currency: 'usd',
+                product_data: {
+                    name: cartItem.idUnique,
+                },
+                unit_amount: (cartItem.precio).toFixed(0),
+            },
+            item.quantity = 1
+            return item;
+        })
+        
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {};
+        const orden=await strapi.services.order.create({ item: {cartDetail, cartTotal, Total_items:cartTotal.length}});
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items,
+            mode: 'payment',
+            success_url: `${MY_DOMAIN}?success=true`,
+            cancel_url: `${MY_DOMAIN}?canceled=true`,
+        })
+        console.log(session)
+        return { id: session.id}
+    }
+}
+     
+   
